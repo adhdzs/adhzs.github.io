6db7fa316d52f2ceb18ce59eeb4f0efd179c92b2
adhdzs <ad.hernandezsa@gmail.com>
2022-10-26 12:32:56 -0500

    Correción ejercicio Números Perfectos

--- js/js4.js
+++ js/js4.js
@@ -98,10 +98,10 @@ function mostrarImg() {
  */
 
 function procesar () {
-     let resultadoTxt = '1';
      let limite = parseInt(document.getElementById('cantidad').value);
      let cantidad = 0;
      let numero = 2;
+     let resultadoTxt = '1';
      
      while (cantidad <= limite) {
           let suma = 1;
@@ -123,3 +123,37 @@ function procesar () {
      let lim = parseInt( document.getElementById('cantidad').value);
      procesar(lim);
 } */
+
+function numPerfecto() {
+     let limite = parseInt(document.getElementById('cantidad').value);
+     let perfectos = 0;
+     let resultado = '';
+     let div = document.getElementById('resultado');
+
+     for ( let n = 2; perfectos < limite; n++) {
+          let suma = 0;
+          for ( let divisor = 1; divisor < n; divisor++ ) {
+               if ( n % divisor == 0 ) {
+                    suma += divisor;
+               }
+          }
+          if ( suma == n ) {
+               resultado += `<p>${ n }</p>\n`;
+               perfectos ++;
+          }
+     }
+     div.innerHTML = resultado;
+}
+
+let numero = 6;
+
+let suma = 0;
+
+for (let d = 1; d < numero; d++) {
+     if ( numero % d == 0 ) {
+          suma += d
+     }
+}
+if ( suma == numero ) {
+     console.log(`Numero perfecto: ${suma}`)
+}
